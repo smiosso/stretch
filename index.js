60592b62e3246fe1453abff9c3d277df723d6eb4
Samantha Tamiosso <samicolognese@gmail.com>
2024-11-27 10:46:40 -0300

    obj created in js

---
+++ index.js
@@ -0,0 +1,39 @@
+let items = [
+    {
+        title: "Bracing maneuver",
+        content: {
+            title: "Muscle group:",
+            description: "Abdomen.",
+            subTitle: "Execution method:",
+            mistakesDescription: "Lie on your back on the floor, feet hip-width apart and flat on the ground, palms facing up, chin tucked in, and shoulder blades retracted. Inhale fully, then forcefully exhale through your mouth, closing your ribs in the process. Keep the ribs closed and contract your abdomen by pulling your belly button inward as if trying to touch it to your spine. Maintain normal breathing without losing the contraction.",
+        },
+        img: "img_alongamentos/0.0-bracing.png",
+    },
+    {
+        title: "Standing calves",
+        text: {
+            title: "Muscle group:",
+            description: "Gastrochnemius and soleus.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Inactive bracing, back leg bent, back heel lifted off the ground, lost plantar arch, and vertical torso not aligning with the incline of the back leg.",
+        },
+        img: "img_alongamentos/1.1-panturrilhas-em-pe.png",
+    },
+    {
+        title: "Bilateral calves on a step or wall bars",
+        text: {
+            title: "Muscle group:",
+            description: "Gastrochnemius and soleus.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Inactive bracing, lumbar hyperlordosis, failure to relax the calf, lost plantar arch, and loss of toe support at the base.",
+        },
+        img: "img_alongamentos/img_alongamentos/1.2-panturrilhas-bilateral-degrau.png",
+    },
+];
+
+for (let i = 0; i < items.length; i++) {
+    let current = items[i];
+
+    
+    
+}
