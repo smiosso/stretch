e3d04ab9a32a57004c8c406deb9aafa3c5039bfd
Samantha Tamiosso <samicolognese@gmail.com>
2024-12-02 16:17:45 -0300

    new excersises

--- index.js
+++ index.js
@@ -3,35 +3,263 @@ let exercises = [
         title: "Bracing maneuver",
         text: {
             title: "Muscle group:",
-            description: "Abdomen.",
+            description: "Lorem ipsum.",
             subTitle: "Execution method:",
-            mistakesDescription: "Lie on your back on the floor, feet hip-width apart and flat on the ground, palms facing up, chin tucked in, and shoulder blades retracted. Inhale fully, then forcefully exhale through your mouth, closing your ribs in the process. Keep the ribs closed and contract your abdomen by pulling your belly button inward as if trying to touch it to your spine. Maintain normal breathing without losing the contraction.",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
         },
         img: "/img_alongamentos/0.0-bracing.png",
         limb: "upper",
-        difficulty: "advanced"
+        difficulty: "begginer"
     },
     {
-        title: "Standing calves",
+        title: "1.1 Standing calves",
         text: {
             title: "Muscle group:",
-            description: "Gastrochnemius and soleus.",
+            description: "Lorem ipsum.",
             subTitle: "Common mistakes:",
-            mistakesDescription: "Inactive bracing, back leg bent, back heel lifted off the ground, lost plantar arch, and vertical torso not aligning with the incline of the back leg.",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
         },
-        img: "/img_alongamentos/1.1-panturrilhas-em-pe.png",
+        img: "/img_alongamentos/1.1.png",
         limb: "lower",
         difficulty: "beginner"
     },
     {
-        title: "Bilateral calves on a step or wall bars",
+        title: "1.2 Bilateral calves on a step or wall bars",
         text: {
             title: "Muscle group:",
-            description: "Gastrochnemius and soleus.",
+            description: "Lorem ipsum.",
             subTitle: "Common mistakes:",
-            mistakesDescription: "Inactive bracing, lumbar hyperlordosis, failure to relax the calf, lost plantar arch, and loss of toe support at the base.",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
         },
-        img: "/img_alongamentos/1.2-panturrilhas-bilateral-degrau.png",
+        img: "/img_alongamentos/1.2.png",
+        limb: "lower",
+        difficulty: "advanced"
+    },
+    {
+        title: "1.3 Unilateral calves on a step or wall bars",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/1.3.png",
+        limb: "lower",
+        difficulty: "advanced"
+    },
+    {
+        title: "1.4 Calf with ring",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/1.4.png",
+        limb: "lower",
+        difficulty: "advanced"
+    },
+    {
+        title: "2.1 Iliopsoas lying down",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/2.1.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "2.2 Iliopsoas standing on wall bars",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/2.2.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "2.3 Iliopsoas kneeling",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/2.3.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "2.4 Iliopsoas and rectus femoris",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/2.4.png",
+        limb: "lower",
+        difficulty: "advanced"
+    },
+    {
+        title: "3.1 Quadril flexed bilateral 90°",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/3.1.png",
+        limb: "lower",
+        difficulty: "advanced"
+    },
+    {
+        title: "3.1 Hip flexed bilateral 90°",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/3.1.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "3.2 Hip flexed bilateral 45°",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/3.2.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "3.3 Hip flexed unilateral",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/3.3.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "3.4 Hip extended",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/3.4.png",
+        limb: "lower",
+        difficulty: "advanced"
+    },
+    {
+        title: "4.1 Embracing rhomboids",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/4.1.png",
+        limb: "upper",
+        difficulty: "begginer"
+    },
+    {
+        title: "4.2 Rhomboids in internal rotation",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/4.2.png",
+        limb: "upper",
+        difficulty: "begginer"
+    },
+    {
+        title: "4.3 Rhomboids in external rotation",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/4.3.png",
+        limb: "upper",
+        difficulty: "begginer"
+    },
+    {
+        title: "4.4 Posterior deltoid",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/4.4.png",
+        limb: "upper",
+        difficulty: "advanced"
+    },
+    {
+        title: "5.1 Standing cervical",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/5.1.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "5.2 Cervical flexors",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/5.2.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "5.3 Upper trapezius",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/5.3.png",
+        limb: "lower",
+        difficulty: "begginer"
+    },
+    {
+        title: "5.4 Diagonal trapezius",
+        text: {
+            title: "Muscle group:",
+            description: "Lorem ipsum.",
+            subTitle: "Common mistakes:",
+            mistakesDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus magna, hendrerit nec purus eu, lobortis mattis diam. Fusce sed leo nisi.",
+        },
+        img: "/img_alongamentos/5.4.png",
         limb: "lower",
         difficulty: "advanced"
     },
@@ -51,13 +279,26 @@ function renderFunction(exercises) {
         containerExercises.appendChild(div);
         div.className = "exercise-container";
 
+        let checkboxTitle = document.createElement("div");
+        div.appendChild(checkboxTitle);
+        checkboxTitle.className = "checkboxTitle";
+
+        // Create a checkbox with a unique ID
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        div.appendChild(checkbox);
+        checkbox.id = `checkbox-${i}`;
+        checkboxTitle.appendChild(checkbox);
 
+        // Create the title and link it to the checkbox with for
         let title = document.createElement("h6");
-        div.appendChild(title);
+        checkboxTitle.appendChild(title);
         title.textContent = current.title;
+
+        // Associate the label with the checkbox and title
+        let label = document.createElement("label");
+        label.setAttribute("for", `checkbox-${i}`);
+        label.appendChild(title);
+        checkboxTitle.appendChild(label);
 
         let icon = document.createElement("i");
         icon.classList.add("far", "fa-circle-down");
